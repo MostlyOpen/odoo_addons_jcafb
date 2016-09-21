255a65f91e337f514c5bc48333380f82cdb24874
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-09-21 13:12:51 -0300

    [ADD] jcafb_2017_consent_forms

---
+++ jcafb_2017_consent_forms/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#                                                                              #
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol                  #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU Affero General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU Affero General Public License for more details.                          #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+################################################################################
+
+{
+    'name': 'JCAF 2017 Consent Forms',
+    'version': '1.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'http://clvsol.com',
+    'description': '''
+This module will install all the JCAF 2017 Consent Forms.
+    ''',
+    'depends': [
+        'jcafb_2017_surveys',
+    ],
+    'data': [
+        'TCP17/survey_jcafb_TCP17.xml',
+        'TCR17/survey_jcafb_TCR17.xml',
+        'TID17/survey_jcafb_TID17.xml',
+    ],
+    'test': [],
+    'installable': True,
+    'active': False,
+}
