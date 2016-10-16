dcd76e6efc3de3d9215146c797ef96bc9ede42f6
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-10-16 17:59:21 -0200

    [IMP] jcafb_2017_consent_forms

---
+++ jcafb_2017_consent_forms/README.rst
@@ -0,0 +1,6 @@
+JCAF 2017 Consent Forms
+=======================
+
+This module will install all the JCAF 2017 Consent Forms.
+
+Developed by `CLVsol <https://clvsol.com>`_.
--- jcafb_2017_consent_forms/__openerp__.py
+++ jcafb_2017_consent_forms/__openerp__.py
@@ -1,41 +1,46 @@
-# -*- encoding: utf-8 -*-
-################################################################################
-#                                                                              #
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol                  #
-#                                                                              #
-# This program is free software: you can redistribute it and/or modify         #
-# it under the terms of the GNU Affero General Public License as published by  #
-# the Free Software Foundation, either version 3 of the License, or            #
-# (at your option) any later version.                                          #
-#                                                                              #
-# This program is distributed in the hope that it will be useful,              #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
-# GNU Affero General Public License for more details.                          #
-#                                                                              #
-# You should have received a copy of the GNU Affero General Public License     #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
-################################################################################
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
 
 {
     'name': 'JCAF 2017 Consent Forms',
-    'version': '1.0',
+    'summary': 'This module will install all the JCAF 2017 Consent Forms.',
+    'version': '2.0.0',
     'author': 'Carlos Eduardo Vercelino - CLVsol',
     'category': 'Generic Modules/Others',
     'license': 'AGPL-3',
     'website': 'http://clvsol.com',
-    'description': '''
-This module will install all the JCAF 2017 Consent Forms.
-    ''',
     'depends': [
-        'jcafb_2017_surveys',
+        'myo_survey',
     ],
     'data': [
-        'TCP17/survey_jcafb_TCP17.xml',
-        'TCR17/survey_jcafb_TCR17.xml',
-        'TID17/survey_jcafb_TID17.xml',
+        'data/survey_jcafb_TCP17.xml',
+        'data/survey_jcafb_TCR17.xml',
+        'data/survey_jcafb_TID17.xml',
     ],
+    'demo': [],
     'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
     'installable': True,
+    'application': False,
     'active': False,
+    'css': [],
 }
