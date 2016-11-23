5a3a224c94bd60878a59bfe2280703cf793eec47
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-11-23 07:00:20 -0200

    [ADD] jcafb_2017_lab_tests

---
+++ jcafb_2017_lab_tests/README.rst
@@ -0,0 +1,6 @@
+JCAF 2017 Surveys
+=================
+
+This module will install all the JCAF 2017 Surveys.
+
+Developed by `CLVsol <https://clvsol.com>`_.
---
+++ jcafb_2017_lab_tests/__openerp__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'JCAF 2017 Lab Tests',
+    'summary': 'This module will install all the JCAF 2017 Lab Tests.',
+    'version': '2.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'http://clvsol.com',
+    'depends': [
+        'myo_survey',
+    ],
+    'data': [
+        'data/survey_jcafb_EAN17.xml',
+        'data/survey_jcafb_EDH17.xml',
+        'data/survey_jcafb_EPC17.xml',
+        'data/survey_jcafb_EPI17.xml',
+        'data/survey_jcafb_EUR17.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
