f0c8ac18508833e4472b31056bc74afe17f63bdd
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-02-15 13:55:43 -0200

    [IMP] myo_lab_test_cst

--- myo_lab_test_cst/wizard/lab_test_anemia_dhc_check_wizard.py
+++ myo_lab_test_cst/wizard/lab_test_anemia_dhc_check_wizard.py
@@ -36,7 +36,83 @@ class LabTestAnemiaDHCCheckWizard(models.TransientModel):
     def do_lab_test_anemia_dhc_check(self):
         self.ensure_one()
 
+        lab_test_request_model = self.env['myo.lab_test.request']
+        lab_test_anemia_dhc_model = self.env['myo.lab_test.anemia_dhc']
+
         for lab_test_anemia_dhc_reg in self.lab_test_anemia_dhc_ids:
             print '>>>>>', lab_test_anemia_dhc_reg.request_code_anemia, lab_test_anemia_dhc_reg.request_code_dhc
+
+            lab_test_anemia_dhc_reg.notes = False
+
+            if lab_test_anemia_dhc_reg.request_code_anemia != 'n/d':
+
+                lab_test_request_search = lab_test_request_model.search([
+                    ('name', '=', lab_test_anemia_dhc_reg.request_code_anemia),
+                ])
+                if lab_test_request_search.id is False:
+                    if lab_test_anemia_dhc_reg.notes is False:
+                        lab_test_anemia_dhc_reg.notes = u'Erro: Codigo da Requisição (Anemia) inválido!'
+                    else:
+                        lab_test_anemia_dhc_reg.notes += u'\nErro: Codigo da Requisição (Anemia) inválido!'
+                else:
+                    if lab_test_anemia_dhc_reg.request_id_anemia.lab_test_type_id.name != \
+                       u'JCAFB 2017 - Exames para detecção de Anemia':
+                        if lab_test_anemia_dhc_reg.notes is False:
+                            lab_test_anemia_dhc_reg.notes = u'Erro: Tipo de Exame da Requisição (Anemia) inválido!'
+                        else:
+                            lab_test_anemia_dhc_reg.notes += u'\nErro: Tipo de Exame da Requisição (Anemia) inválido!'
+                    if lab_test_anemia_dhc_reg.request_id_anemia.patient_id.name != \
+                       lab_test_anemia_dhc_reg.lab_test_person_id.name:
+                        if lab_test_anemia_dhc_reg.notes is False:
+                            lab_test_anemia_dhc_reg.notes = u'Erro: Pessoa da Requisição (Anemia) inválida!'
+                        else:
+                            lab_test_anemia_dhc_reg.notes += u'\nErro: Pessoa da Requisição (Anemia) inválida!'
+
+                    lab_test_anemia_dhc_search = lab_test_anemia_dhc_model.search([
+                        ('request_code_anemia', '=', lab_test_anemia_dhc_reg.request_code_anemia),
+                    ])
+                    if len(lab_test_anemia_dhc_search) > 1:
+                        if lab_test_anemia_dhc_reg.notes is False:
+                            lab_test_anemia_dhc_reg.notes = u'Erro: Codigo da Requisição (Anemia) Duplicado!'
+                        else:
+                            lab_test_anemia_dhc_reg.notes += u'\nErro: Codigo da Requisição (Anemia) Duplicado!'
+
+            if lab_test_anemia_dhc_reg.request_code_dhc != 'n/d':
+
+                lab_test_request_search = lab_test_request_model.search([
+                    ('name', '=', lab_test_anemia_dhc_reg.request_code_dhc),
+                ])
+                if lab_test_request_search.id is False:
+                    if lab_test_anemia_dhc_reg.notes is False:
+                        lab_test_anemia_dhc_reg.notes = u'Erro: Codigo da Requisição (DHC) inválido!'
+                    else:
+                        lab_test_anemia_dhc_reg.notes += u'\nErro: Codigo da Requisição (DHC) inválido!'
+                else:
+                    if lab_test_anemia_dhc_reg.request_id_dhc.lab_test_type_id.name != \
+                       u'JCAFB 2017 - Exames - Diabetes, Hipertensão Arterial e Hipercolesterolemia':
+                        if lab_test_anemia_dhc_reg.notes is False:
+                            lab_test_anemia_dhc_reg.notes = u'Erro: Tipo de Exame da Requisição (DHC) inválido!'
+                        else:
+                            lab_test_anemia_dhc_reg.notes += u'\nErro: Tipo de Exame da Requisição (DHC) inválido!'
+                    if lab_test_anemia_dhc_reg.request_id_dhc.patient_id.name != \
+                       lab_test_anemia_dhc_reg.lab_test_person_id.name:
+                        if lab_test_anemia_dhc_reg.notes is False:
+                            lab_test_anemia_dhc_reg.notes = u'Erro: Pessoa da Requisição (DHC) inválida!'
+                        else:
+                            lab_test_anemia_dhc_reg.notes += u'\nErro: Pessoa da Requisição (DHC) inválida!'
+
+                    lab_test_anemia_dhc_search = lab_test_anemia_dhc_model.search([
+                        ('request_code_dhc', '=', lab_test_anemia_dhc_reg.request_code_dhc),
+                    ])
+                    if len(lab_test_anemia_dhc_search) > 1:
+                        if lab_test_anemia_dhc_reg.notes is False:
+                            lab_test_anemia_dhc_reg.notes = u'Erro: Codigo da Requisição (DHC) Duplicado!'
+                        else:
+                            lab_test_anemia_dhc_reg.notes += u'\nErro: Codigo da Requisição (DHC) Duplicado!'
+
+            if lab_test_anemia_dhc_reg.notes is False:
+                lab_test_anemia_dhc_reg.state = 'checked'
+            else:
+                lab_test_anemia_dhc_reg.state = 'draft'
 
         return True
